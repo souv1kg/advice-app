3f00e8272111a495aedb9b94a2b04ae44df269b7
Abhishek Ghosh <abhishekghosh018@gmail.com>
2022-03-12 21:57:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [state, setState] = useState({ advice: "" });
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  const getAdvice = () => {
+    //The random api updates once every 5 seconds. Instead, we can call the API using the api id and set its randomness.
+    const id = Math.floor(Math.random() * 100) - 1;
+    axios
+      .get(`https://api.adviceslip.com/advice/${id}`)
+      .then((res) => {
+        const { advice } = res.data.slip;
+        setState({ advice });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{state.advice}</h1>
+        <button className="button" onClick={getAdvice}>
+          <span>Give me advice!</span>
+        </button>
+      </div>
     </div>
   );
 }
